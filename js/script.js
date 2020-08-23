a1eca26dea9e537866baacecf2ea87ddc1bfde32
Leonardo Bozzi <bozzileonardo8@gmail.com>
2020-08-23 19:06:35 -0300

    first version

---
+++ js/script.js
@@ -0,0 +1,65 @@
+var popup = document.getElementById('modal');
+var juarez = document.getElementById('modalJuarez');
+var leonardo = document.getElementById('modalLeonardo');
+var caio = document.getElementById('modalCaio');
+var guilherme = document.getElementById('modalGuilherme');
+
+
+
+//modal alex
+function popupOnAlex(){
+  popup.style.display = "block";
+}
+
+function popupOffAlex(){
+  popup.style.display = "none";
+}
+
+popupOffAlex();
+
+//modal juarez
+function popupOnJuarez(){
+  juarez.style.display = "block";
+}
+
+function popupOffJuarez(){
+  juarez.style.display = "none";
+}
+
+popupOffJuarez();
+
+//modal leonardp
+function popupOnLeonardo(){
+  leonardo.style.display = "block";
+}
+
+function popupOffLeonardo(){
+  leonardo.style.display = "none";
+}
+
+popupOffLeonardo();
+
+//modal caio
+function popupOnCaio(){
+  caio.style.display = "block";
+}
+
+function popupOffCaio(){
+  caio.style.display = "none";
+}
+ 
+popupOffCaio();
+
+//modal guilherme
+function popupOnGuilherme(){
+  guilherme.style.display = "block";
+}
+
+function popupOffGuilherme(){
+  guilherme.style.display = "none";
+}
+
+popupOffGuilherme();
+
+
+console.log(popupOnAlex)
