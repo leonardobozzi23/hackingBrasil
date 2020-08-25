fb23549b73a53429923e32605de6c1e70f424820
Leonardo Bozzi <bozzileonardo8@gmail.com>
2020-08-25 02:08:17 -0300

    versao 1 page pronta sem responsividade

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var juarez = document.getElementById('modalJuarez');
 var leonardo = document.getElementById('modalLeonardo');
 var caio = document.getElementById('modalCaio');
 var guilherme = document.getElementById('modalGuilherme');
-
+var inscrever = document.getElementById('modalInscrever');
+var parabens = document.getElementById('modalParabens');
 
 
 //modal alex
@@ -61,5 +62,27 @@ function popupOffGuilherme(){
 
 popupOffGuilherme();
 
+//modal inscrição
 
-console.log(popupOnAlex)
+function popupOnInscrever(){
+  inscrever.style.display = "block";
+}
+
+function popupOffInscrever(){
+  inscrever.style.display = "none";
+}
+
+popupOffInscrever();
+
+
+//modal parabens
+
+function popupOnParabens(){
+  parabens.style.display = "block";
+}
+
+function popupOffParabens(){
+  parabens.style.display = "none";
+}
+
+popupOffParabens();
