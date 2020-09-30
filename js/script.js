57ca46769e6fd952f07c77431a507223006f29f5
Leonardo Bozzi <bozzileonardo8@gmail.com>
2020-09-30 13:57:28 -0300

    versao final

--- js/script.js
+++ js/script.js
@@ -1,62 +1,61 @@
-var popup = document.getElementById('modal');
-var juarez = document.getElementById('modalJuarez');
-var leonardo = document.getElementById('modalLeonardo');
-var caio = document.getElementById('modalCaio');
-var guilherme = document.getElementById('modalGuilherme');
-var inscrever = document.getElementById('modalInscrever');
-var parabens = document.getElementById('modalParabens');
-
+var popup = document.getElementById("modal");
+var juarez = document.getElementById("modalJuarez");
+var leonardo = document.getElementById("modalLeonardo");
+var caio = document.getElementById("modalCaio");
+var guilherme = document.getElementById("modalGuilherme");
+var inscrever = document.getElementById("modalInscrever");
+var parabens = document.getElementById("modalParabens");
 
 //modal alex
-function popupOnAlex(){
+function popupOnAlex() {
   popup.style.display = "block";
 }
 
-function popupOffAlex(){
+function popupOffAlex() {
   popup.style.display = "none";
 }
 
 popupOffAlex();
 
 //modal juarez
-function popupOnJuarez(){
+function popupOnJuarez() {
   juarez.style.display = "block";
 }
 
-function popupOffJuarez(){
+function popupOffJuarez() {
   juarez.style.display = "none";
 }
 
 popupOffJuarez();
 
 //modal leonardo
-function popupOnLeonardo(){
+function popupOnLeonardo() {
   leonardo.style.display = "block";
 }
 
-function popupOffLeonardo(){
+function popupOffLeonardo() {
   leonardo.style.display = "none";
 }
 
 popupOffLeonardo();
 
 //modal caio
-function popupOnCaio(){
+function popupOnCaio() {
   caio.style.display = "block";
 }
 
-function popupOffCaio(){
+function popupOffCaio() {
   caio.style.display = "none";
 }
- 
+
 popupOffCaio();
 
 //modal guilherme
-function popupOnGuilherme(){
+function popupOnGuilherme() {
   guilherme.style.display = "block";
 }
 
-function popupOffGuilherme(){
+function popupOffGuilherme() {
   guilherme.style.display = "none";
 }
 
@@ -64,25 +63,42 @@ popupOffGuilherme();
 
 //modal inscrição
 
-function popupOnInscrever(){
+function popupOnInscrever() {
   inscrever.style.display = "block";
 }
 
-function popupOffInscrever(){
+function popupOffInscrever() {
   inscrever.style.display = "none";
 }
 
 popupOffInscrever();
 
-
 //modal parabens
 
-function popupOnParabens(){
+function popupOnParabens() {
   parabens.style.display = "block";
 }
 
-function popupOffParabens(){
+function popupOffParabens() {
   parabens.style.display = "none";
 }
 
 popupOffParabens();
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", scrollToIdOnClick);
+});
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute("href");
+  const to = document.querySelector(id).offsetTop;
+
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+}
